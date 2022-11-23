93430f658378c15b4a71abc706118db7a36e9823
moonstruck19 <hungnp.21it@vku.udn.vn>
2022-11-23 13:18:22 +0700

    first commit

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -73,9 +73,6 @@
 				<div class="inner">
 					<div class="row">
 						<div class="col-lg-6 col-12">
-							<div class="left">
-								<p>Copyright © {{date('Y')}} <a href="https://github.com/Prajwal100" target="_blank">Prajwal Rai</a>  -  All Rights Reserved.</p>
-							</div>
 						</div>
 						<div class="col-lg-6 col-12">
 							<div class="right">
